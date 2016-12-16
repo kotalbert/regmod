f13ee2c11109dced569c9053c6973ed1be494250
Pawel <pawel@daniluk.waw.pl>
2016-12-16 22:47:19 +0100

    project completed

--- project/mt.R
+++ project/mt.R
@@ -4,61 +4,43 @@
 library(ggplot2)
 library(dplyr)
 library(car)
+library(GGally)
 
 data(mtcars)
 head(mtcars)
 
-ggplot(mtcars, aes(mpg)) + geom_histogram()
-dev.off()
-
-# Data seems to be normal
+# The target variable - check for normality
+ggplot(mtcars, aes(mpg)) + geom_histogram(fill="lightblue", color="black")
 shapiro.test(mtcars$mpg)
 
-# Check the relation 
-ggplot(mtcars, aes(factor(am), mpg)) + geom_boxplot()
-dev.off()
-
-# Check other variables
-
-# cylinders
-ggplot(mtcars, aes(factor(cyl), mpg)) + geom_boxplot()
-dev.off()
-
-# Displacement
-ggplot(mtcars, aes(hp, disp)) + geom_point()
-dev.off()
-
-# Horsepower
-ggplot(mtcars, aes(hp, mpg)) + geom_point()
-dev.off()
-
-# Weight
-ggplot(mtcars, aes(wt, mpg)) + geom_point()
-dev.off()
-
-# Drat?
-ggplot(mtcars, aes(drat, mpg)) + geom_point()
-dev.off()
-
-# Qsec
-ggplot(mtcars, aes(qsec, mpg)) + geom_point()
-dev.off()
-
-# Gear
-ggplot(mtcars, aes(factor(gear), mpg)) + geom_boxplot()
-dev.off()
-
-# Carb
-ggplot(mtcars, aes(factor(carb), mpg)) + geom_boxplot()
-dev.off()
-
-# calculate correcations
+# check correlated variables
 cor(mtcars)[1,]
-fit1 <- lm(mpg ~ factor(am), data=mtcars)
-fit2 <- update(fit1, mpg~factor(am)+factor(cyl))
-fit3 <- update(fit1, mpg~factor(am)+factor(cyl)+wt)
-fit4 <- update(fit1, mpg~factor(am)+factor(cyl)+wt+hp)
 
-anova(fit1, fit2, fit3, fit4)
+# Transform cyl and am to factors
+# Select only highly correlated factors
+mtc2 <- mtcars %>% 
+	dplyr::mutate(cylf = factor(cyl), amf=factor(am)) %>%
+	dplyr::select(mpg, cylf, amf,  disp, hp, wt)
 
-vif(fit1)
+# pairs plot for selected variables
+ggpairs(mtc2)
+
+# Model selection procedure
+fit1 <- lm(mpg ~ amf, data=mtc2)
+fit2 <- update(fit1, mpg~amf+cylf)
+fit3 <- update(fit1, mpg~amf+cylf+disp)
+fit4 <- update(fit1, mpg~amf+cylf+disp+wt)
+fit5 <- update(fit1, mpg~amf+cylf+disp+wt+hp)
+anova(fit1, fit2, fit3, fit4, fit5)
+summary(fit5)
+fit6 <- update(fit1, mpg~amf+wt+hp)
+anova(fit1, fit2, fit3, fit4, fit5, fit6)
+summary(fit6)
+vif(fit5)
+vif(fit6)
+
+# Residuals diagnostics
+plot(fit5, which=1)
+res <- fit5$residuals
+hist(fit5$residuals)
+shapiro.test(fit5$residuals)
